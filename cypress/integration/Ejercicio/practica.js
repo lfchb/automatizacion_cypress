70156ffd37527f5c97386627a63d60053400c7d2
Liliana Fernanda Chacha Burgos <lchachab@iess.ad>
2022-05-27 10:59:28 -0500

    practica

--- cypress/integration/Ejercicio/practica.js
+++ cypress/integration/Ejercicio/practica.js
@@ -18,28 +18,71 @@ describe('Conjunto de casos de prueba', function()
     });
 
     //Caso de prueba 2: Agregar dos productos al carrito
-    it('Agregar dos productos al carrito', function(){
-        //Obtener la lista de productos
-        //cy.get('.inventory_list > :nth-child(1)').as('Productos')
-
+    it.skip('Agregar dos productos al carrito', function(){
+        //Obtenemos la lista de productos
         cy.get(':nth-child(2) > :nth-child(1) > #inventory_container').as('Productos')
-
-    
         
         //Iteramos la lista de productos
         cy.get('@Productos')
         .find('.inventory_item_name')
         .each(($el,index,$list) => {
-            //cy.log('Se ha encontrado el elemento buscado: '+$el.wrap.name)
-            //cy.get('@Productos').eq(0).contains('Add to cart').click()
-            //cy.get('@Productos').eq(1).contains('Add to cart').click()
-            //cy.log('Se ha encontrado el elemento buscado: '+index)
-
-            //if(index===1){
-                cy.get('@Productos').eq(index).contains('Add to cart').click()
-            //}
+            //Mostramos los productos encontrados
+            //cy.log('Se ha encontrado el elemento buscado: '+ index)
+            
+            //Seleccionados 2 productos:
+            if(index<=1){
+                cy.get('@Productos').contains('Add to cart').click()
+            }
         })
+    });
+
+    //Caso de prueba 3: Visualizar el carrito
+    it.skip('Agregar dos productos al carrito y visualizar el carrito', function(){
+        //Obtenemos la lista de productos
+        cy.get(':nth-child(2) > :nth-child(1) > #inventory_container').as('Productos')
         
+        //Iteramos la lista de productos
+        cy.get('@Productos')
+        .find('.inventory_item_name')
+        .each(($el,index,$list) => {
+            //Mostramos los productos encontrados
+            //cy.log('Se ha encontrado el elemento buscado: '+ index)
+            
+            //Seleccionados 2 productos:
+            if(index<=1){
+                cy.get('@Productos').contains('Add to cart').click()
+            }
+        })
+
+        cy.get('.shopping_cart_link').click()
+    });
+
+    //Caso de prueba 4: Completamos la información de despacho
+    it('Agregar dos productos al carrito, visualizamos el carrito y completamos informacion de despacho', function(){
+        //Obtenemos la lista de productos
+        cy.get(':nth-child(2) > :nth-child(1) > #inventory_container').as('Productos')
+        
+        //Iteramos la lista de productos
+        cy.get('@Productos')
+        .find('.inventory_item_name')
+        .each(($el,index,$list) => {
+            //Mostramos los productos encontrados
+            //cy.log('Se ha encontrado el elemento buscado: '+ index)
+            
+            //Seleccionados 2 productos:
+            if(index<=1){
+                cy.get('@Productos').contains('Add to cart').click()
+            }
+        })
+
+        //Visualizar el carrito de compras
+        cy.get('.shopping_cart_link').click()
+        //
+        cy.get('[data-test="checkout"]').click()
+        //Completar la información de despacho
+        cy.get('[data-test="firstName"]').type('Liliana')
+        cy.get('[data-test="lastName"]').type('Chacha')
+        cy.get('[data-test="postalCode"]').type('170502')
     });
 }
 )
